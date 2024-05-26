aadb2ed6178d5d770e2122f47f4760f600eb6722
Bruno Pimentel <bruno@pimentel.dev>
2024-05-26 19:22:03 -0300

    chore: update kotlin to 2.0.0

--- mobile/androidApp/build.gradle.kts
+++ mobile/androidApp/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
--- mobile/shared/build.gradle.kts
+++ mobile/shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
@@ -73,7 +74,7 @@ kotlin {
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "pro.aeternum"
+    namespace = "pro.aeternum.common"
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res", "src/commonMain/resources")
